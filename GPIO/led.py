97cd83a73c2eccf76014c7e7a2bf0db6e773b624
Roger Chiu <roger.chiu@academicos.udg.mx>
2023-10-10 19:46:11 -0600

    modified:   led.py

--- GPIO/led.py
+++ GPIO/led.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
-import Rpi.GPIO as GPIO
+import RPi.GPIO as GPIO  # Import the RPi.GPIO library for controlling GPIO pins
 import time
 
-LedPin = 4
+LedPin = 4  # Define the GPIO pin (BCM mode) to which the LED is connected
+
+# Set the GPIO mode to BCM (Broadcom SOC channel numbering)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LedPin,GPIO.OUT)
+
+# Set the LedPin as an output pin
+GPIO.setup(LedPin, GPIO.OUT)
+
+# Turn on the LED by setting the output to HIGH
 GPIO.output(LedPin, GPIO.HIGH)
-time.slep(5)
+
+# Wait for 5 seconds
+time.sleep(5)  # Note the corrected function name "time.sleep" instead of "time.slep"
+
+# Turn off the LED by setting the output to LOW
 GPIO.output(LedPin, GPIO.LOW)
+
+# Clean up and release the GPIO resources
 GPIO.cleanup()
 
 
